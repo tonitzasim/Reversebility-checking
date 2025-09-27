1f3c118acbafd4f999106b890b4fe5a511fc1cc9
tonitzasim <67192616+tonitzasim@users.noreply.github.com>
2025-09-27 18:54:07 +0200

    Update Controllo_spanning_tree.cpp

--- Controllo_spanning_tree.cpp
+++ Controllo_spanning_tree.cpp
@@ -1,7 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main()  {
-    int n;
+    
+	//dichiarazioni iniziali e input
+	
+	int n;
     cin>>n;
     double pesi [n][n];
     vector<int> adj[n];
@@ -10,69 +13,12 @@ int main()  {
     vector<double> forwards(n,0);
     queue<int> q; 
 
-    srand(time(0));
-	double somma=0;
-	double prob_zero= 1/ (double) 2;
-	double distribuzione[n];
-	double accetta[n][n], iniziale[n][n];
-
-
-	for(int i=0;i<n;i++)
-	{
-		distribuzione[i]=rand()%100+1;
-		somma+=distribuzione[i];
-	}
-	
-		for(int i=0;i<n;i++)
-	{
-		distribuzione[i]/=double(somma);
-	}
-
-
-	//generiamo la matrice randomica iniziale
-
-	for(int i=0;i<n;i++){
-	somma=0;		
-		for(int j=0;j<n;j++){
-			if( rand()%100 / (double) 100 < prob_zero&&i!=((j-1)%n) && i!=((j+1)%n) ){
-				iniziale[i][j]=0;
-			}
-			else {
-			iniziale[i][j]=rand()%100+1;
-		somma+=iniziale[i][j];
-	}
-		}
-		for(int j=0;j<n;j++){
-			iniziale[i][j]/=somma;
-		}
-	}
-	//calcoliamo la matrice con la probabilità di accettazione
-
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
-			
-			if(i==j || iniziale[i][j]==0|| iniziale[j][i]==0)
-				accetta[i][j]=0;
-			else {
-				accetta[i][j]= min((double) 1, ( distribuzione[j]*iniziale[j][i]/( distribuzione[i] * iniziale[i][j] ) ) );
-			}
+			pesi[i][j];
 		}
 	}
-	
-
-		//moltiplichiamo elemento per elemnto le due matrice e normalizziamo 
-	//gli elementi sulla diagonale
-	
-	for(int i=0;i<n;i++){
-		somma=0;
-		for(int j=0;j<n;j++)
-		{
-			pesi[i][j]=iniziale[i][j]*accetta[i][j];
-			somma+=pesi[i][j];
-			
-		}
-		pesi[i][i]-=somma-1;
-	}
+	//costruiamo le liste di adiacenza
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
 			if(pesi[i][j]>0){
@@ -80,7 +26,7 @@ int main()  {
 			}	
 		}
 	}
-
+//facciamo una bfs per calcolare i vettori di forwards e backwards
     visited[0] = true;
     q.push(0);
     
@@ -105,8 +51,9 @@ int main()  {
         }
 }
 
+    //controlliamo la condizione di Kolmogorov sui cicli fondamntali
     
-    bool reversibile=true;
+	bool reversibile=true;
     
     for(int i=1;i<n;i++){
     	if(visited[i]=false){
@@ -121,8 +68,7 @@ int main()  {
 	}
 	
 	cout<<reversibile;
-    
-    
-    
+	
     return 0;
 }
+
