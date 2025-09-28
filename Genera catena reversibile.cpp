35b95961f35812846cab6e54eacd73881f536953
tonitzasim <67192616+tonitzasim@users.noreply.github.com>
2025-09-28 19:17:51 +0200

    Update Genera catena reversibile.cpp

--- Genera catena reversibile.cpp
+++ Genera catena reversibile.cpp
@@ -1,15 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+
+	//dichiarazioni iniziali e input
+	
 	int n;
 	srand(time(0));
 	double somma=0;
 	double prob_zero= 1/ (double) 2;
+	cout<<"Inserisci numero di stati: "
 	cin>>n;
+	cout<<endl;
 	double distribuzione[n];
-	double pesi[n][n], accetta[n][n], reversibile[n][n];
+	double pesi[n][n], accetta[n][n], iniziale[n][n];
 	
-	//generaimo una distribuzione all'equilibrio randomica
+	//generiamo una distribuzione all'equilibrio randomica
 	for(int i=0;i<n;i++)
 	{
 		distribuzione[i]=rand()%100;
@@ -25,51 +30,52 @@ int main(){
 	somma=0;		
 		for(int j=0;j<n;j++){
 			if( rand()%100 / (double) 100 < prob_zero&&i!=((j-1)%n) && i!=((j+1)%n) ){
-				pesi[i][j]=0;
+				iniziale[i][j]=0;
 			}
 			else {
-			pesi[i][j]=rand()%100;
-		somma+=pesi[i][j];
+			iniziale[i][j]=rand()%100;
+		somma+=iniziale[i][j];
 	}
 		}
 		for(int j=0;j<n;j++){
-			pesi[i][j]/=somma;
+			iniziale[i][j]/=somma;
 		}
 	}
 	
-	//calcoliamo la matrice con le probabilità di accettazione
+	//calcoliamo la matrice con le probabilitÃ  di accettazione
 	
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
 			
-			if(i==j || pesi[i][j]==0|| pesi[j][i]==0)
+			if(i==j || iniziale[i][j]==0|| iniziale[j][i]==0)
 				accetta[i][j]=0;
 			else {
-				accetta[i][j]= min((double) 1, ( distribuzione[j]*pesi[j][i]/( distribuzione[i] * pesi[i][j] ) ) );
+				accetta[i][j]= min((double) 1, ( distribuzione[j]*iniziale[j][i]/( distribuzione[i] * iniziale[i][j] ) ) );
 			}
 		}
 	}
 	
-	//moltiplichiamo elemento per elemnto le due matrice e normalizziamo 
-	//gli elementi sulla diagonale
+	//moltiplichiamo elemento per elemento le due matrice e normalizziamo 
+	//gli elementi sulla diagonale per ottenere una matrice reversibile
 	
 	for(int i=0;i<n;i++){
 		somma=0;
 		for(int j=0;j<n;j++)
 		{
-			reversibile[i][j]=pesi[i][j]*accetta[i][j];
-			somma+=reversibile[i][j];
+			pesi[i][j]=iniziale[i][j]*accetta[i][j];
+			somma+=pesi[i][j];
 			
 		}
-		reversibile[i][i]-=somma-1;
+		pesi[i][i]-=somma-1;
 	}
 	
-	
+	//stampiamo i risultati finali
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++)
 		{
-			cout<<reversibile[i][j]<<" ";
+			cout<<pesi[i][j]<<" ";
 		}
 		cout<<endl;
 	}
 }
+
